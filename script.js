b1ad0d034018d0a24ebbc0a16fad7e31fbd29736
Harsh Sharma <hsharma4090@gmail.com>
2025-02-08 17:35:31 +0530

    Changes done

---
+++ script.js
@@ -0,0 +1,93 @@
+function OnMouseOver1(){
+   let x=document.getElementById("box1");
+   x.innerHTML = "Harsh";
+   x.style.backgroundColor ="rgba(128, 128, 128, .7)";
+   x.style.color = "white";
+   x.style.border = "solid white 2px";
+   x.style.width= "22rem";
+   x.style.height = "16rem";
+}
+function OnMouseLeave1(){
+    let x=document.getElementById("box1");
+    x.innerHTML = "Name";
+    x.style.backgroundColor ="rgba(128, 128, 128, 0.5)";
+    x.style.color = "skyblue";
+    x.style.border = "none";
+    x.style.width= "20rem";
+    x.style.height = "14rem";
+ }
+function OnMouseOver2(){
+   let x=document.getElementById("box2");
+   x.innerHTML = "Male";
+   x.style.backgroundColor ="rgba(128, 128, 128, .7)";
+   x.style.color = "white";
+   x.style.border = "solid white 2px";
+   x.style.width= "22rem";
+   x.style.height = "16rem";
+}
+function OnMouseLeave2(){
+    let x=document.getElementById("box2");
+    x.innerHTML = "Gender";
+    x.style.backgroundColor ="rgba(128, 128, 128, 0.5)";
+    x.style.color = "skyblue";
+    x.style.border = "none";
+   x.style.width= "20rem";
+   x.style.height = "14rem";
+   
+
+ }
+function OnMouseOver3(){
+   let x=document.getElementById("box3");
+   x.innerHTML = "Nalla";
+   x.style.backgroundColor ="rgba(128, 128, 128, .7)";
+   x.style.color = "white";
+   x.style.border = "solid white 2px";
+   x.style.width= "22rem";
+   x.style.height = "16rem";
+}
+function OnMouseLeave3(){
+    let x=document.getElementById("box3");
+    x.innerHTML = "Occupation";
+    x.style.backgroundColor ="rgba(128, 128, 128, 0.5)";
+    x.style.color = "skyblue";
+    x.style.border = "none";
+    x.style.width= "20rem";
+    x.style.height = "14rem";
+ }
+function OnMouseOver4(){
+   let x=document.getElementById("box4");
+   x.innerHTML = "Html, Css, JavaScript";
+   x.style.backgroundColor ="rgba(128, 128, 128, .7)";
+   x.style.color = "white";
+   x.style.border = "solid white 2px";
+   x.style.width= "22rem";
+   x.style.height = "16rem";
+}
+function OnMouseLeave4(){
+    let x=document.getElementById("box4");
+    x.innerHTML = "Skills";
+    x.style.backgroundColor ="rgba(128, 128, 128, 0.5)";
+    x.style.color = "skyblue";
+    x.style.border = "none";
+    x.style.width= "20rem";
+    x.style.height = "14rem";
+ }
+function OnMouseOver5(){
+   let x=document.getElementById("box5");
+   x.innerHTML = "Btech Cse";
+   x.style.backgroundColor ="rgba(128, 128, 128, .7)";
+   x.style.color = "white";
+   x.style.border = "solid white 2px";
+   x.style.width= "22rem";
+   x.style.height = "16rem";
+}
+function OnMouseLeave5(){
+    let x=document.getElementById("box5");
+    x.innerHTML = "Degree";
+    x.style.backgroundColor ="rgba(128, 128, 128, 0.5)";
+    x.style.color = "skyblue";
+    x.style.border = "none";
+    x.style.width= "20rem";
+    x.style.height = "14rem";
+ }
+ 
